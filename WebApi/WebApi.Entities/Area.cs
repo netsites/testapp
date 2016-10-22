88ca7bd69cef94f7712870cad95c07e83a529497
yoadad <yoadad@gmail.com>
2016-10-22 16:00:37 -0500

    20161022-155513 Agregando código fuente del web api

---
+++ WebApi/WebApi.Entities/Area.cs
@@ -0,0 +1,26 @@
+namespace WebApi.Entities
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Area
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Area()
+        {
+            Empleados = new HashSet<Empleado>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(64)]
+        public string Nombre { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Empleado> Empleados { get; set; }
+    }
+}

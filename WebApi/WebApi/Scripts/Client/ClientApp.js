6827ff98fe7ad281b6f36922284d6f5893b98908
yoadad <yoadad@gmail.com>
2016-10-29 16:21:56 -0500

    agregando edit y borrar Area

---
+++ WebApi/WebApi/Scripts/Client/ClientApp.js
@@ -0,0 +1,90 @@
+﻿var ClientApp = ClientApp || {};
+
+(function ($, ClientApp) {
+    ClientApp.selectedAreaId = 0;
+
+    ClientApp.GetAreas = function () {
+        var url = "/api/areas";
+        $.getJSON(url,{},ClientApp.ManageGetAreasResponse);
+    };
+
+    ClientApp.ManageGetAreasResponse = function (data) {
+            var tempFn = doT.template($('#areasListTemplate').html());            
+            var resultText = tempFn(data);
+            $('#grid').html(resultText);
+    };
+
+    ClientApp.ShowEditModal = function (areaId) {
+        ClientApp.selectedAreaId = areaId;
+        $('#txtEditNombreArea').val($('#area-' + areaId + ' td:eq(1)').text());
+        $('#modal').modal('show');
+    };
+    ClientApp.ShowDeleteModal = function (areaId) {
+        ClientApp.selectedAreaId = areaId;
+        $('#deleteModal').modal('show');
+    };
+
+    ClientApp.GetAreas();
+
+    ClientApp.AddArea = function (areaName) {
+        var url = "/api/areas";
+        var data = { Nombre: areaName };
+        $.post(url, data, ClientApp.ManageAddOrEditAreaResponse, 'json');
+    };
+
+    ClientApp.EditArea = function (areaId,areaName) {
+        var url = "/api/areas/"+areaId;
+        var data = {Id:areaId, Nombre: areaName };
+
+        $.ajax({
+            type: 'put',
+            url: url,
+            data: data,
+            success: ClientApp.ManageAddOrEditAreaResponse
+        });
+        //$.put(url, data, ClientApp.ManageAddOrEditAreaResponse, 'json');
+    };
+
+    ClientApp.ManageAddOrEditAreaResponse = function (data) {
+        ClientApp.GetAreas();
+
+        $('#modal').modal('hide');
+    };
+
+    ClientApp.CancelDelete = function () {
+        $('#deleteModal').modal('hide');
+    };
+
+    ClientApp.DeleteArea = function (areaId) {
+        var url = "/api/areas/" + areaId;
+        $.ajax({
+            type: 'delete',
+            url: url,
+            success: ClientApp.ManageDeleteAreaResponse
+        });
+    };
+
+    ClientApp.ManageDeleteAreaResponse = function () {
+        ClientApp.GetAreas();
+        $('#deleteModal').modal('hide');
+    };
+
+
+
+    $('#btnAdd').click(function () {
+
+        var areaName = $('#txtNombreArea').val();
+        ClientApp.AddArea(areaName);
+    });
+
+    $('#btnEdit').click(function () {
+        var areaName = $('#txtEditNombreArea').val();
+        ClientApp.EditArea(ClientApp.selectedAreaId, areaName);
+    });
+
+
+    $('#btnDelete').click(function () {
+        ClientApp.DeleteArea(ClientApp.selectedAreaId);
+    });
+
+})(jQuery,ClientApp);
---
+++ WebApi_Publish/Scripts/Client/ClientApp.js
@@ -0,0 +1,34 @@
+﻿var ClientApp = ClientApp || {};
+
+(function ($, ClientApp) {
+
+    ClientApp.GetAreas = function () {
+        var url = "/api/areas";
+        $.getJSON(url,{},ClientApp.ManageGetAreasResponse);
+    };
+
+    ClientApp.ManageGetAreasResponse = function (data) {
+            var tempFn = doT.template($('#areasListTemplate').html());            
+            var resultText = tempFn(data);
+            $('#grid').html(resultText);
+    };
+
+    ClientApp.GetAreas();
+
+    ClientApp.AddArea = function (areaName) {
+        var url = "/api/areas";
+        var data = { Nombre: areaName };
+        $.post(url,data,ClientApp.ManageAddAreaResponse,'json');
+    };
+
+    ClientApp.ManageAddAreaResponse = function(data){
+        ClientApp.GetAreas();
+    };
+
+    $('#btnAdd').click(function () {
+
+        var areaName = $('#txtNombreArea').val();
+        ClientApp.AddArea(areaName);
+    });
+
+})(jQuery,ClientApp);
